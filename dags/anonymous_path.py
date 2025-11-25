2168c1d73863b6a4f4dae14b43b04d2f83b68ae4
MokaMoka77 <tbuiquang103@gmail.com>
2025-11-25 16:02:46 +0700

    update Path_file

--- dags/anonymous_path.py
+++ dags/anonymous_path.py
@@ -6,4 +6,5 @@ class Path_Folder:
         self.processed_folder_path = Path_Folder.processed_folder_path
         self.raw_folder_path = Path_Folder.raw_folder_path
 
+
     #   path_folder = Path_Folder()
